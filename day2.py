c8610c270883d77a093fb181311e3364946cccf2
Ieva <ieva.maksvytyte@gmail.com>
2023-08-23 00:23:39 +0100

    day2-cleaned

--- day2.py
+++ day2.py
@@ -16,24 +16,9 @@ def is_J_D_name(name: str) -> bool:
     """Returns True if name has JD initials"""
 
     name = name.split(' ')
-    if name[0] == 'J' and name[-1] == 'D':
+    if name[0][0] == 'J' and name[-1][0] == 'D':
         return True
     return False
-
-
-def find_suspects(o_data: list, suspects: list, o_i_data: list):
-
-    narrowed_suspects = []
-    for person in suspects:
-        for row in o_data:
-            if person["customerid"] == row["customerid"]:
-                order_id = row["orderid"]
-                break
-        for row in o_i_data:
-            if row["orderid"] == order_id:
-                if row["sku"] == "DLI1464":
-                    narrowed_suspects.append(person)
-    return narrowed_suspects
 
 
 if __name__ == "__main__":
@@ -46,6 +31,16 @@ if __name__ == "__main__":
     customers = customers[customers["name"].apply(is_J_D_name)]
 
     # combining three datasets to find their order items
-    customers = customers.merge(orders, how="left", on="customerid")["orderid"]
+    customers = customers.merge(orders, how="left", on="customerid")[["name", "phone", "orderid", "ordered", "shipped"]]
+
+    # only keeping in-store orders
+    customers = customers[customers["ordered"] == customers["shipped"]].drop(columns="shipped")
+
+    # merging for order details
+    customers = customers.merge(order_details, how="left", on="orderid")
+
+    # finalising suspects who ordered Drip coffee
+    customers = customers[customers["sku"] == "DLI1464"]
 
     print(customers)
+    # 335  Jeremy Davis  212-771-8924   7409.0  2017-04-05 12:49:41  DLI1464    1        7.73
