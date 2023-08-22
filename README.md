af57c6cc0721124c2438da05aaf503878b45d90b
Ieva <ieva.maksvytyte@gmail.com>
2023-08-22 23:36:36 +0100

    second-commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# HELLO!
+
+🕎🎊Welcome to Hanukkah of Data, 2022, solution in 2023!🎊🕎
+
+You will find my highly subjective solutions, which often equipped every tool at my disposal (`cmd+F` included 🏃💨).
+
+## Setup
+
+1. Please set up a virtual environment before attempting my solutions, using:\
+`python3 -m venv venv`, or other preferred methods.
+
+2. Please install the `requirements.txt` file, using:\
+`pip install -m requirements.txt`, or other preferred methods.
+
+
+Finally, please enjoy, and have a go yourself. The tapestry at the end is worth it.
+
+(Did anyone else think that the suspects describing various animals was an inside joke by the creators, alluding to the tapestry being so stained initially that no one could tell what animal it was?)
--- day1.py
+++ day1.py
@@ -1,3 +1,8 @@
+"""Day1: f you wanted a phone number that was easy-to-remember, you could get a number that spelled
+something using the letters printed on the phone buttons: like 2 has “ABC”, and 3 “DEF”, etc. And I
+guess this person had done that, so if you dialed the numbers corresponding to the letters in their name,
+it would call their phone number!"""
+
 import pandas as pd
 
 CUSTOMER_DATA = "./noahs-csv/noahs-customers.csv"
@@ -13,8 +18,14 @@ def generate_phone_numbers(name: str) -> str:
                 number += decoder[decode]
     return number
 
+
+def find_suspect(customers: pd.DataFrame):
+    """Finds the suspect"""
+
+    customer
+
 if __name__ == "__main__":
-    customers =
+    customers = pd.read_csv(CUSTOMER_DATA)
 
     for customer in customers:
         last_name = customer["name"].split(' ')[1]
