ba839b22f9e75621f1c6ad6acba37d70986a972a
Ieva <ieva.maksvytyte@gmail.com>
2023-08-23 12:58:48 +0100

    final-day-8-clean

--- day8.py
+++ day8.py
@@ -1,33 +1,33 @@
+"""she gave it to an acquaintance of hers who collects all sorts of junk. Apparently he owns
+an entire set of Noah’s collectibles! He probably still has the rug, even."""
 import pandas as pd
 
-CUSTOMER_DATA = "./noahs-csv/noahs-customers.csv"
 ORDER_DATA = "./noahs-csv/noahs-orders.csv"
 ORDER_ITEMS_DATA = "./noahs-csv/noahs-orders_items.csv"
 PRODUCTS_DATA = "./noahs-csv/noahs-products.csv"
 
-people_data = pd.read_csv(CUSTOMER_DATA)
-order_data = pd.read_csv(ORDER_DATA)
-o_i_data = pd.read_csv(ORDER_ITEMS_DATA)
-product_data = pd.read_csv(PRODUCTS_DATA)
-
 # new date: 2019-06-01
 
-def filter_by_noahs_collectibles(o_i_data, order_data, product_data):
+def filter_by_noahs_collectibles(order_details, orders, products):
+    """Find the collectibles and sales counts, find the collector"""
 
-    order_data = o_i_data.merge(order_data, how="inner", on="orderid")
-    order_data = order_data.drop(columns=["shipped", "items"])
-
-    merge_with_products = order_data.merge(product_data, how="left", on="sku")
-    merge_with_products = merge_with_products.drop(columns=["unit_price", "total", "wholesale_cost"])
+    orders = order_details.merge(orders, how="inner", on="orderid")
+    merge_with_products = orders.merge(products, how="left", on="sku")
 
     # collectibles will probs have "Noah's" in the name
     merge_with_products = merge_with_products[merge_with_products["desc"].str.contains("Noah's")]
 
     # which show up most frequently?
-    individuals = merge_with_products["customerid"].value_counts().reset_index().sort_values(by="count")
+    individuals = merge_with_products["customerid"].value_counts().reset_index().sort_values(by="count").tail()
 
     print(individuals)
-    # Three times as much purchases of "Noah's" as the second top
+    # One individual has three times as much of "Noah's" purchases as the second top
     #4308,"Travis Bartlett","2527B Adam Clayton Powell Jr Blvd","Manhattan, NY 10039","1942-07-22","929-906-5980"
 
-filter_by_noahs_collectibles(o_i_data, order_data, product_data)
+
+if __name__ == "__main__":
+    orders = pd.read_csv(ORDER_DATA)
+    order_details = pd.read_csv(ORDER_ITEMS_DATA)
+    products = pd.read_csv(PRODUCTS_DATA)
+
+    filter_by_noahs_collectibles(order_details, orders, products)
